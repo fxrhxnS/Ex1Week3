e8de688f1cd60cb10b62952c17fa90ff2cbd6ccb
fxrhxnS <72841327+fxrhxnS@users.noreply.github.com>
2020-10-28 10:57:22 +0800

    Add files via upload

---
+++ Ex1Week3.java
@@ -0,0 +1,108 @@
+import java.util.Scanner;
+
+public class Ex1Week3{
+	public static void main(String [] nilaiTerima){
+
+		int noInt = 10,x;
+
+		// System.out.println(noInt ==10 > "sama : " : "tak sama");
+
+		if (noInt ==10)
+			x=3;
+		else
+			x=19;
+
+		//ternary operator
+		x = noInt == 18 ? 3 : 19;
+
+		System.out.println("x is "+ x);
+
+
+		// String nama;
+		// int nombor;
+		// double nomborDouble;
+
+		// Scanner input = new Scanner(System.in);
+
+		// System.out.println("Enter a number: ");
+		// nombor = input.nextInt();
+		// System.out.println("Number is "+nombor);
+
+		// // System.out.println("Enter a name: ");// boleh letak satu string sahaja
+		// // nama = input.next();//means tk bleh space bleh letak satu perkataan shj
+		// // System.out.println("Name is "+nama);
+
+		// input.netLine();
+
+		// System.out.println("Enter a name: ");
+		// nama = input.nextLine();//bleh lebih dr satu string
+		// System.out.println("Name is "+nama);
+
+		// System.out.println("Enter a double: ");
+		// nomborDouble = input.nextDouble();
+		// System.out.println("Number is "+nomborDouble);
+
+		// input.close();
+
+
+
+		// int[] tataInt={10,20,30,40};
+		// for (int i=0;i<tataInt.length;i++ ) {
+		// 	System.out.println(tataInt[i]);
+			
+		// }
+		// for (int t: tataInt ) {
+		// 	System.out.println("for each: " + t);
+			
+		// }
+
+
+
+
+
+		//int noInt=0;
+		//System.out.println("Nombor int  "+noInt);
+		//int[] tataInt = new int[4];
+		//for (int i=0; i<tataInt.length; i++){
+			//System.out.println(tataInt[i]);
+
+
+
+		// final static char CHARSAYA =53;
+		// 	System.out.println("Char " + CHARSAYA);
+		// 	CHARSAYA=100;
+		// 	System.out.println("Char " + CHARSAYA)
+
+
+
+		// int noInt = 123;
+		// int noInt2 = 456;
+		// double noDouble = 123.456;
+		// System.out.format("%6d dan %d" , noInt , noInt2);
+		// System.out.format("%7.2f",noDouble);
+		// System.out.printf("\n\tRM%7.3f dan %6d ", noDouble , noInt);
+		// 6 space ( d = space )
+
+
+
+		// int sum = 0;
+		// for(int i = 0; i < nilaiTerima.length;i++)
+		// {
+		// 	System.out.println(" Nilai " + nilaiTerima[i]);
+		// 	sum += Integer.parseInt(nilaiTerima[i]);
+		// 	// consider string menjadi number.
+		// 	System.out.println(" Nilai Sum " + sum);
+		// }
+
+
+		// System.out.println("Nilai 1 : " + nilaiTerima[0]);
+		// System.out.println("Nilai 1 : " + nilaiTerima[1]);
+		// System.out.println("Nilai 1 : " + nilaiTerima[2]);
+
+	}
+}
+
+
+//int [] tataInt= new[4];
+
+//for
